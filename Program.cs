8900a770d5218b4c40c9c3a5f02834c1ab0fa594
Po <counterparty@outlook.com>
2023-04-30 08:27:36 +0200

    UseCors

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ var logger = new LoggerConfiguration()
     .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
-            
+
 builder.Services.AddCors();
 
 builder.Services.AddEndpointsApiExplorer();
@@ -17,10 +17,15 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSingleton<IRpcClient>(new RPCClient(new StaticRestClientFactory()));
 builder.Services.AddSingleton<IDiscordBotClient>(new DiscordClient(new StaticDiscordSocketClientFactory(logger, AppSettings.DiscordSettings)));
 builder.Services.AddCarter();
-
+ 
 await using var app = builder.Build();
 
-app.UseCors(corsPolicyBuilder => corsPolicyBuilder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+// global cors policy
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true) // allow any origin
+    .AllowCredentials()); // allow credentials
 
 //config endpoints with Carter:
 //app.MapGet("/", () => "Hello! This is .NET 6 Minimal API App Service").ExcludeFromDescription();
